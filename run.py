99dfb7e186b0a513dde0514d2707bebe91617acd
wintermute242 <wintermute242@protonmail.com>
2021-12-11 19:20:43 -0500

    Small fix to handling the case of no config file

--- run.py
+++ run.py
@@ -16,10 +16,12 @@ if not os.path.exists('neriak.ini'):
     if os.path.exists('example_neriak.ini'):
         print("No configuration file found. Creating copy from 'example_neriak.ini'")
         shutil.copyfile('example_neriak.ini','neriak.ini')
+        print("Please setup 'neriak.ini' and run this program again.")
     else:
         print("No configuration file 'neriak.ini' or example file 'example_neriak.ini' was found.")
         print("Please create these files or download a new copy from the repo.")
-        os.exit(1)
+    
+    os.exit(1)
 
 
 # Dynamically load the module specified on the command line. Assumes Module and Class name are the same.
